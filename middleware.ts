482b428b168ecf28867560a99cb1ed73a42b9d87
Jan Piotrowski <piotrowski+github@gmail.com>
2024-03-12 19:12:34 +0000

    Try LibSQL

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,15 @@
 import { PrismaClient } from "@prisma/client";
-import { PrismaNeon } from "@prisma/adapter-neon";
-import { Pool } from '@neondatabase/serverless'
+import { PrismaNeon } from "@prisma/adapter-libsql";
+import { createClient } from '@libsql/client'
 
 import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(req: NextRequest): Promise<NextResponse> {
-  const neon = new Pool({ connectionString: process.env.DATABASE_URL })
-  const adapter = new PrismaNeon(neon)
+
+  const libsql = createClient({
+    url: `${process.env.DATABASE_URL}`,
+  })
+  const adapter = new PrismaLibsql(libsql)
   const prisma = new PrismaClient({ adapter })
   const user = await prisma.user.findFirst();
   return NextResponse.next();
